6d252ae9053fbce3fa10e93b3b2f4b8e224c0f37
ali1x <71478250+ali1x@users.noreply.github.com>
2021-01-16 05:12:00 +0200

    Add files via upload

---
+++ colorGen.js
@@ -0,0 +1,10 @@
+let charList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+function generate() {
+    let colorGenerated = ''
+    for (let x = 0; x < 6; x++) {
+        colorGenerated += charList[Math.floor(Math.random() * (charList.length - 1))]
+    }
+    document.getElementById('output').innerHTML = `Background Color: #${colorGenerated}`
+    document.body.style.backgroundColor = `#${colorGenerated}`
+}
