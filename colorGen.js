f0c0d8c376a002b18a97280494a24fd5f0ed5ff5
ali1x <71478250+ali1x@users.noreply.github.com>
2021-01-23 07:04:57 +0200

    Add files via upload

--- colorGen.js
+++ colorGen.js
@@ -1,5 +1,6 @@
 const charList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 const generateBtn = document.querySelector('#gene')
+const searchBar = document.querySelector('#hex-search')
 
 function generate() {
     let colorGenerated = ''
@@ -12,4 +13,10 @@ function generate() {
 
 generateBtn.addEventListener('click', () => {
     generate()
+})
+
+searchBar.addEventListener('change', () => {
+    let color = document.getElementById('hex-search').value
+    document.body.style.backgroundColor = `#${color}`
+    document.getElementById('output').innerHTML = `Background Color: #${color}`
 })
