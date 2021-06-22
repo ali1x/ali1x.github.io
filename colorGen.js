3c8ba30b8075531ca5e8e4a3022d147d27cdd544
ali1x <71478250+ali1x@users.noreply.github.com>
2021-06-22 21:24:07 +0300

    Update colorGen.js

--- colorGen.js
+++ colorGen.js
@@ -2,7 +2,7 @@ const charList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C
 const generateBtn = document.querySelector('#gene')
 const searchBar = document.querySelector('#hex-search')
 
-function generate() {
+const generate = () => {
     let colorGenerated = ''
     for (let x = 0; x < 6; x++) {
         colorGenerated += charList[Math.floor(Math.random() * (charList.length - 1))]
@@ -13,10 +13,4 @@ function generate() {
 
 generateBtn.addEventListener('click', () => {
     generate()
-})
-
-searchBar.addEventListener('change', () => {
-    let color = document.getElementById('hex-search').value
-    document.body.style.backgroundColor = `#${color}`
-    document.getElementById('output').innerHTML = `Background Color: #${color}`
 })
